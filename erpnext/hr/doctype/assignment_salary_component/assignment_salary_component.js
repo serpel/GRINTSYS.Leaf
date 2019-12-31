73df4f6b920e8733145854d44950a9ba04c4e72f
GersonCaballero <gersoncaballero306@gmail.com>
2019-12-31 11:46:56 -0600

    Creando estructura y funcion para asignar componentes salariales

--- erpnext/hr/doctype/assignment_salary_component/assignment_salary_component.js
+++ erpnext/hr/doctype/assignment_salary_component/assignment_salary_component.js
@@ -5,4 +5,19 @@ frappe.ui.form.on('Assignment Salary Component', {
 	// refresh: function(frm) {
 
 	// }
+	validate_assignment_Salary_Component: function(frm){
+		if(frm.doc.amended_from){
+			frappe.call({
+				method: 'validate_assignment_Salary_Component',
+				args: {},
+				callback: function(r) {
+				},
+				doc: frm.doc,
+				freeze: true,
+				freeze_message: 'Validating assignment Salary Component...'
+			});
+		}else{
+			frm.fields_dict.attendance_detail_html.html("");
+		}
+	},
 });
--- erpnext/hr/doctype/assignment_salary_component/assignment_salary_component.py
+++ erpnext/hr/doctype/assignment_salary_component/assignment_salary_component.py
@@ -10,6 +10,26 @@ from frappe.model.document import Document
 class AssignmentSalaryComponent(Document):
 	
 	def validate(self):
-		details = frappe.get_all("Salary Detail",["name","salary_component","abbr","statistical_component","is_tax_applicable","is_flexible_benefit", "variable_based_on_taxable_salary", "depends_on_payment_days", "deduct_full_tax_on_selected_payroll_date", "default_amount", "additional_amount", "tax_on_flexible_benefit", "tax_on_additional_salary"], filters = {"name": "fb7e010297"})
-		salary_slip = frappe.get_all("Salary Slip")
-		frappe.throw(_("details: {}, nomina: {}".format(details, salary_slip)))
+		self.status = self.get_status()
+	
+	def get_status(self):
+		if self.docstatus == 0:
+			status = "Saved"
+		elif self.docstatus == 1:
+			self.validate_assignment_Salary_Component()
+			status = "Finished"
+		return status
+	
+	def on_cancel(self):
+		if self.docstatus == 2:
+			self.status = "Cancelled"
+	
+	def validate_assignment_Salary_Component(self):
+		employees = frappe.get_all("Employee Detail Salary Component", ["employee","moneda", "parent"], filters = {"parent": self.name})
+		for item in employees:
+			salary_slip = frappe.get_all("Salary Slip", ["name"], filters={"payroll_entry":self.payroll_entry, "employee":item.employee})
+			salary_detail = frappe.get_all("Salary Detail", ["name", "parent"])
+			salary_component = {'salary_component':self.salary_component, 'type':self.type, 'amount':item.amount}
+			frappe.throw("{} {}".format(salary_detail, salary_component))
+			# for salary in salary_slip:
+			# 	frappe.db.sql("""INSERT INTO tabSalary Detail (salary_component, parent) VALUES ({},{})""".format(salary_component, salary.name)
